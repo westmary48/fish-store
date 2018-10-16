7ed699b0f033e4bd20d9f0b6ffe9f8885fe55628
Mary West <westmarye1219@gmail.com>
2018-10-16 18:18:09 -0500

    added jq

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,12 @@
 // Filter fish that are "on sale"
 
 // Add fish to "Basket"
+
+$.get('../db/fishes.json')
+.done((data) => {
+    console.log(data);
+})
+
+.fail((error) => {
+    console.error({error});
+});

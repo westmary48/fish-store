ae33b51eec074679f024c2e11bcc1f3882cc9846
Mary West <westmarye1219@gmail.com>
2018-10-16 20:26:43 -0500

    added toggle

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,10 +6,9 @@ const writeFishes = (arrayOfFishes) => {
     let domString = '';
     arrayOfFishes.forEach((fish) => {
         domString += `
-        <div class="${fish.onSale ? 'on-sale' : ''} on-sale fish card col-md-6 col-md-offset-3"> 
+        <div class="${fish.onSale ? 'on-sale' : ''} fish card col-md-6 col-md-offset-3"> 
         <div class="thumbnail">
-            <img src="${fish.imageSoure}"
-                alt="" width="40%">
+            <img src="${fish.imageSoure}" alt="" width="40%">
             <div class="caption">
                 <h3 id="thumbnail-label">${fish.name}</h3>
                 <p>$
@@ -22,34 +21,37 @@ const writeFishes = (arrayOfFishes) => {
         </div>
         </div>`
 
-    })
-    // Write to the available div
-
-    $("#available").append(domString);
-    bindEvents();
-
-}
-
-    const bindEvents = () => {
-        $(".add").on('click', (e) => {
-            //what is the div that has the fish
-        const fishToMove =  $(e.target).closest('.fish');
-            // move it to the 'snagged' div
-            $("#snagged").append(fishToMove);
-            // button text => Remove from Basket | change class - 'add' + 'remove'
-            $(e.target).text('Remove from Basket').addClass('remove').removeClass('add');
-        });
-    };
-
-
-//Load fish
-$.get('../db/fishes.json')
-    .done((data) => {
-        console.log(data);
-        writeFishes(data.fishes);
-    })
-    .fail((error) => {
-        console.error({ error });
     });
 
+    $("#available").append(domString);
+
+}
+    $('body').on('click', 'button.add', (e) => {
+        const fishToMove = $(e.target).closest('.fish');
+        $("#snagged").append(fishToMove);
+        $(e.target).text('Remove from Basket').addClass('remove').removeClass('add');
+    })
+    $('body').on('click', 'button.remove', (e) => {
+        const fishToMove = $(e.target).closest('.fish');
+        $("#available").append(fishToMove);
+        $(e.target).text('Add To Basket').addClass('add').removeClass('remove');
+    })
+
+$("#show-sale").click(() => {
+// alert('SALE FISHHHHH');
+// all divs with the class fish, give me just the ones WITHOUT the class 'on sale' and HIDE
+$(".fish").not(".on-sale").toggle();
+
+});
+
+    //Load fish
+    $.get('../db/fishes.json')
+        .done((data) => {
+            console.log(data);
+            writeFishes(data.fishes);
+        })
+        .fail((error) => {
+            console.error({ error });
+        });
+    
 
